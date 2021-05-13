60968e6c0b18689e9f7b1c73cfc28f01fcf5fac7
bmatlak <bartosz.matlak@op.pl>
2021-05-13 15:37:54 +0200

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,108 @@
+const toolBtns = document.querySelectorAll("button.tool");
+const saveBtn = document.querySelector("button.save");
+const removeBtn = document.querySelector("button.remove");
+const uploadFileInput = document.querySelector("input.file");
+const editor = document.querySelector("div.editor");
+const label = document.querySelector("label.option");
+const toolbar = document.querySelector("div.toolbar");
+const fileInfo = document.querySelector("p.fileInfo");
+
+const formatText = (command, button) => {
+  editor.focus();
+  button.classList.toggle("active");
+  document.execCommand(command, false, null);
+};
+
+const saveFile = (e) => {
+  e.preventDefault();
+  const fileName = window.prompt("Filename: ", "document");
+  if (fileName) {
+    const content = editor.innerHTML;
+    const data = {
+      content,
+      fileName,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch("/saving", options).then((response) => {
+      response.json();
+      if (response.status < 400) {
+        alert("File successfully written!");
+      } else {
+        alert("Something went wrong!");
+      }
+    });
+  } else {
+    alert("Name the file !");
+  }
+};
+const openFile = () => {
+  if (uploadFileInput.files.length > 0) {
+    const reader = new FileReader();
+    console.log();
+    fileInfo.textContent = `Opened file: ${uploadFileInput.files[0].name}`;
+    reader.addEventListener("load", function () {
+      const result = JSON.parse(reader.result);
+      label.classList.add("disabled");
+      editor.innerHTML = result.content;
+      uploadFileInput.disabled = true;
+    });
+
+    reader.readAsText(uploadFileInput.files[0]);
+  }
+};
+const checkStyles = () => {
+  const isBold = document.queryCommandState("Bold");
+  const isItalic = document.queryCommandState("italic");
+  const isList = document.queryCommandState("insertUnOrderedList");
+  document.designMode = "on";
+  for (let button of toolBtns) {
+    let command = button.dataset["command"];
+    if (isBold && isItalic && command != "insertUnOrderedList") {
+      button.classList.add("active");
+    } else if (isBold && command == "bold") {
+      button.classList.add("active");
+    } else if (isItalic && command == "italic") {
+      button.classList.add("active");
+    } else if (isList && command == "insertUnOrderedList") {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+    if (editor.innerHTML == "") {
+      button.classList.remove("active");
+      document.execCommand("removeFormat", false, null);
+    }
+  }
+
+  document.designMode = "off";
+};
+
+const clear = () => {
+  uploadFileInput.value = "";
+  uploadFileInput.disabled = false;
+  editor.innerHTML = "";
+  fileInfo.textContent = "";
+  label.classList.remove("disabled");
+  for (let button of toolBtns) {
+    button.classList.remove("active");
+  }
+};
+
+for (let button of toolBtns) {
+  let command = button.dataset["command"];
+  button.addEventListener("click", () => formatText(command, button));
+}
+
+removeBtn.addEventListener("click", clear);
+saveBtn.addEventListener("click", saveFile);
+uploadFileInput.addEventListener("change", openFile);
+editor.onmouseup = checkStyles;
+editor.onkeyup = checkStyles;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+app.set("view engine", "hbs");
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json({ limit: "1mb" }));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "textEditor" });
+});
+
+app.post("/saving", (req, res) => {
+  const fs = require("fs");
+  const data = req.body;
+  const fileName = data.fileName;
+  delete data.fileName;
+  const path = `./documents/${fileName}.json`;
+  fs.writeFile(path, JSON.stringify(data), (err) => {
+    if (err) throw err;
+    console.log("The file was saved!");
+  });
+  res.json({ message: "Recieved !" });
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
